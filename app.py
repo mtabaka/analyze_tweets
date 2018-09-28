095d21936f6801e62a3ef8b0cc5833299065ed29
mtabaka <matthew.tabaka@gmail.com>
2018-09-28 17:20:32 -0500

    developer acct

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def update_twitter():
     # Create dictionary to hold text and label entities
     tweet_dict = {"text": [], "label": []}
 
-    mentions = api.search(q="@mattdtabaka Analyze:")
+    mentions = api.search(q="@MattTabaka Analyze:")
     print(mentions)
     words = []
     try:
